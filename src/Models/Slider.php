12096080f7d599829c03243c71f884d8649c92db
Enas <enasasmar882@gmail.com>
2023-02-21 15:10:04 +0200

    slider

---
+++ src/Models/Slider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Corals\Modules\Slider\Models;
+
+use Corals\Foundation\Models\BaseModel;
+use Corals\Foundation\Transformers\PresentableTrait;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Slider extends BaseModel
+{
+    use PresentableTrait, LogsActivity;
+
+    /**
+     *  Model configuration.
+     * @var string
+     */
+    public $config = 'slider.models.slider';
+
+    protected $casts = [
+        'init_options' => 'array'
+    ];
+
+    protected $guarded = ['id'];
+
+    public function setKeyAttribute($value)
+    {
+        $this->attributes['key'] = \Str::slug($value);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('status', 'active');
+    }
+
+    public function slides()
+    {
+        return $this->hasMany(Slide::class);
+    }
+
+    public function activeSlides()
+    {
+        return $this->hasMany(Slide::class)->where('status', 'active');
+    }
+}
